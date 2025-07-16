23256139a3a36cf954873256575cef1cb14240d0
Replit-is-bad <ylchia_hill@hotmail.com>
2025-07-16 08:29:23 +0000

    Jul 16, 2025, 8:29 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,8 +3,8 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  FileList("blue")
-  stroke()
+  File("blue")
+  stroke('')
   // write your codes here
  
   // End Basic shape testing
