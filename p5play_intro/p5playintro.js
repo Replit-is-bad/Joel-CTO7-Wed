03e6fb8d84c8cd49a4f82c9375513cd11154087b
Replit-is-bad <ylchia_hill@hotmail.com>
2025-07-23 07:45:37 +0000

    Jul 23, 2025, 7:45 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -44,5 +44,5 @@ function draw() {
   fill(0,0,255);
   textSize(14);
   text("ball : (" + int(ball.x) + "," + int(ball.y) + ")" , 10,20);
-  text("mouse : (" + int(mouse.x) + "," + int(mouse.y) + ")" , 10,20);
+  text("mouse : (" + int(mouse.x) + "," + int(mouse.y) + ")" , 10,40);
 }
