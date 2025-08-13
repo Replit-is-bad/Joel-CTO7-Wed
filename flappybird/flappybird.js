eee8238cf1cf56bdab1f065849263cba24fc95f9
Replit-is-bad <ylchia_hill@hotmail.com>
2025-08-13 08:18:53 +0000

    Aug 13, 2025, 8:18 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -4,6 +4,7 @@ let flapMidImg, bg, base;
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('flappybird/assets/background-day.png')
+    base = load
 }
 
 function setup() {
