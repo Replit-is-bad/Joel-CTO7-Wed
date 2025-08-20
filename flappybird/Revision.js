234b2a5321c17195e0254b976f840caf0509a6ed
Replit-is-bad <ylchia_hill@hotmail.com>
2025-08-20 08:32:56 +0000

    Aug 20, 2025, 8:32 AM

--- flappybird/Revision.js
+++ flappybird/Revision.js
@@ -5,7 +5,7 @@ const pie = 3.1415;
 let oh = 2092389483453476473654673;
 
 function setup() {
-    for (let i = 1; i < 4; i = i++ ) {
+    for (let i=1; i<4; i = i++ ) {
         consol.log(i);
     }
 }
