003ebaa64923f7100639162781483876efe58483
Replit-is-bad <ylchia_hill@hotmail.com>
2025-08-20 08:09:02 +0000

    Aug 20, 2025, 8:09 AM

--- flappybird/flappybird2.js
+++ flappybird/flappybird2.js
@@ -48,7 +48,7 @@ function draw() {
         bird.image = flapUpImg;
         bird.rotation = -30;  
     }
-    else if ( bi)
+    else if ( bird.vel.y > 1)
     fill('blue');
     textSize(14);
     text('vel.y :  ' + bird.vel.y.toFixed(2) , 10 ,20);
