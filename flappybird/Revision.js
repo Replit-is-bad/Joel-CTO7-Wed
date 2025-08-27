909d32c6aedf10836396351b7ed991ddc9351bf6
Replit-is-bad <ylchia_hill@hotmail.com>
2025-08-27 07:34:42 +0000

    Aug 27, 2025, 7:34 AM

--- flappybird/Revision.js
+++ flappybird/Revision.js
@@ -10,11 +10,11 @@
 //     }
 // }
 
-i = 0
-while (i <= 10) {
-  console.log(i);
-  i =i + 1
-}
+// i = 0
+// while (i <= 10) {
+//   console.log(i);
+//   i =i + 1
+// }
 
 // let classNahIdWin = ["Joel","Oliver","Ethan","Kayla"];
 
