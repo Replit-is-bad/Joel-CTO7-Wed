d9fad8dedf04f54dc9da5ada03393a458818f59f
Replit-is-bad <ylchia_hill@hotmail.com>
2025-08-27 07:33:21 +0000

    Aug 27, 2025, 7:33 AM

--- flappybird/Revision.js
+++ flappybird/Revision.js
@@ -4,11 +4,11 @@
 // const pie = 3.1415;
 // let oh = 2092389483453476473654673;
 
-// function setup() {
-//     for (let i=1; i<=3; i++ ) {
-//         console.log(i);
-//     }
-// }
+function setup() {
+    for (let i=1; i<=3; i++ ) {
+        console.log(i);
+    }
+}
 
 // i = 0
 // while (i <= 10) {
@@ -18,28 +18,28 @@
 
 // let classNahIdWin = ["Joel","Oliver","Ethan","Kayla"];
 
-function setup() {
-    new Canvas(400,600);
-    bird = new Sprite();
-    bird.x = width/2;
-    bird.y = 200;
-    bird.width =30;
-    bird.height =30;
-    bird.img = flapMidImg;
+// function setup() {
+//     new Canvas(400,600);
+//     bird = new Sprite();
+//     bird.x = width/2;
+//     bird.y = 200;
+//     bird.width =30;
+//     bird.height =30;
+//     bird.img = flapMidImg;
 
-    bird.collider = "dynamic";
-    bird.mass =2;
-    bird.drag = 0.5 ;
-    bird.bouciness =0.02;
-    world.gravity.y = 10;
+//     bird.collider = "dynamic";
+//     bird.mass =2;
+//     bird.drag = 0.5 ;
+//     bird.bouciness =0.02;
+//     world.gravity.y = 10;
 
 
-    floor = new Sprite();
-    floor.x = 200;
-    floor.y = height - 20;
-    floor.width =400;
-    floor.height =125;
-    floor.img = base;
+//     floor = new Sprite();
+//     floor.x = 200;
+//     floor.y = height - 20;
+//     floor.width =400;
+//     floor.height =125;
+//     floor.img = base;
 
-    floor.collider ='static';
-}
+//     floor.collider ='static';
+// }
