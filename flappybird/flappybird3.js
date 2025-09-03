6220849a78c95210f27951ba2ecbf75d6b57935b
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-03 08:25:53 +0000

    Sep 3, 2025, 8:25 AM

---
+++ flappybird/flappybird3.js
@@ -0,0 +1,100 @@
+let bird, floor;
+let flapMidImg, bg, base;
+let flapUpImg, flapDownImg
+let pipeGroup;
+let pipe;
+let bottomPipe, topPipe;
+
+function preload() {
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+    flapDownImg = loadImage('assets/yellowbird-downflap.png');
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+
+    pipe = loadImage('assets/pipe-green.png');//preload img for pipe
+
+
+}
+
+function setup() {
+    new Canvas(400,600);
+    bird = new Sprite();
+    bird.x = width/2;
+    bird.y = 200;
+    bird.width =30;
+    bird.height =30;
+    bird.img = flapMidImg;
+
+    bird.collider = "dynamic";
+    bird.mass =2;
+    bird.drag = 0.5 ;
+    bird.bouciness =0.02;
+    world.gravity.y = 10;
+
+
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width =400;
+    floor.height =125;
+    floor.img = base;
+
+    floor.collider ='static';
+
+    pipeGroup = new Group();
+}
+
+function draw() {
+    image(bg,0,0,width,height);
+    
+    if(kb.presses('space') || mouse.presses()) {
+        bird.vel.y = -5;
+        bird.sleeping = false;
+    }
+
+    if (bird.vel.y < - 1) {
+        bird.image = flapUpImg;
+        bird.rotation = -30;  
+    }
+    else if ( bird.vel.y > 1) {
+        bird.image = flapDownImg;
+        bird.rotation = 30;
+    }
+    else {
+        bird.image = flapMidImg;
+        bird.rotation = 0;
+    }
+
+    if (frameCount == 1) {
+        spwanPipePair();
+    }
+
+     
+
+    fill('blue');
+    textSize(14);
+    text('vel.y :  ' + bird.vel.y.toFixed(2) , 10 ,20);
+    text('isMoving : ' + bird.isMoving, 10 , 40);
+    text('sleeping : ' + bird.sleeping, 10 ,60);
+} 
+
+function spwanPipePair() { 
+
+    let midY = height/2;
+    let gap = 50;
+
+    bottomPipe = new Sprite(400, midY  + gap/2 + 200, 52, 320, 'static');
+    bottomPipe.img = pipe;
+
+    pipeGroup.add(bottomPipe);
+    
+
+
+    topPipe = new Sprite(400, midY - gap*2 - 200, 52, 320, 'static');
+    topPipe.img = pipe;
+    topPipe.rotation = 180;
+
+    pipeGroup.add(topPipe);
+    pipeGroup.layer = 0;
+}
