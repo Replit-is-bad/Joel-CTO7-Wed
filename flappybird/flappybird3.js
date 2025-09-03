57a619a032b1d0e882843afd70ea857351dc2652
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-03 08:26:38 +0000

    Sep 3, 2025, 8:26 AM

--- flappybird/flappybird3.js
+++ flappybird/flappybird3.js
@@ -91,7 +91,7 @@ function spwanPipePair() {
     
 
 
-    topPipe = new Sprite(400, midY - gap*2 - 200, 52, 320, 'static');
+    topPipe = new Sprite(400, midY - gap/2 - 200, 52, 320, 'static');
     topPipe.img = pipe;
     topPipe.rotation = -180;
 
