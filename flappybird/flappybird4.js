6c465c0b8d5c8c9e72808f1cbb45ae134fe4552a
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-10 08:31:15 +0000

    Sep 10, 2025, 8:31 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -101,7 +101,7 @@ function draw() {
 
     if(bird.collides(pipeGroup) || bird.collides(floor)) {
         gameoverLabel = new Sprite(width/2 , height/2);
-        
+        gameoverLabel = gameo
     }
 
 } 
