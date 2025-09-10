f19c9c161a808151f7c217d915beb8891c3dbbce
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-10 08:53:25 +0000

    Sep 10, 2025, 8:53 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -53,7 +53,7 @@ function setup() {
 
     //start screen Sprite
 
-    startScreenLabel = new Sprite(width/2, height/2)
+    startScreenLabel = new Sprite(width/2, height/2, 50, 50,)
 }
 
 function draw() {
