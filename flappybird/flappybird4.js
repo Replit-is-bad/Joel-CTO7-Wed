5e1745931729fb40f56ee4f016ad447601fc43b5
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-17 07:46:11 +0000

    Sep 17, 2025, 7:46 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -62,6 +62,10 @@ function setup() {
 
 function draw() {
     image(bg,0,0,width,height);
+    if (kb.presses('space') || mouse.presses()) {
+            startGame = true;
+            startScreenLabel.visible = false;
+        }
     if (startGame){
         if(kb.presses('space') || mouse.presses()) {
             bird.vel.y = -5;
