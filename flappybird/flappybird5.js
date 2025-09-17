2549845d2c0a32c0b80473440568d709bf30ba1b
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-17 08:34:56 +0000

    Sep 17, 2025, 8:34 AM

--- flappybird/flappybird5.js
+++ flappybird/flappybird5.js
@@ -192,6 +192,6 @@ function drawScore(x,y,digitWidth,digitHeight) {
 
         let xPos = startX + i * digitWidth;
 
-        let digitSprite = new scoreDigits.Sprite(x)
+        let digitSprite = new scoreDigits.Sprite(xPos,y,d)
     }
 }
