87ab700d445c1acb50ba3677b1b1f1856472ec0e
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-24 08:35:41 +0000

    Sep 24, 2025, 8:35 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -35,7 +35,7 @@ function preload() {
         numberImages[i] = loadImage('assets/' + i + '.png');
     }
 
-    flapSound = createAudio('assets/')
+    flapSound = createAudio('assets/flappybird/assets/sfx_wing.mp3')
     pointSound = createAudio('assets/')
     failSound = createAudio('assets/')
 }
