4c5b5af99e69916bb4d61744aca31d67e851e150
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-24 09:06:20 +0000

    Sep 24, 2025, 9:06 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -181,6 +181,8 @@ function draw() {
                 startScreenLabel.visible = true;
                 startScreenLabel.x = bird.x;
                 startScreenLabel.y = height / 2 - 50;
+
+                loop();
             } , 3000);
         }
 
