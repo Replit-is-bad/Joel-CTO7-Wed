4454c687f3660ea175b3f34d4cb716c9ccde15c9
Replit-is-bad <ylchia_hill@hotmail.com>
2025-09-24 08:01:59 +0000

    Sep 24, 2025, 8:01 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -126,7 +126,9 @@ function draw() {
         }
 
         for(let pipe of pipeGroup) {
-            
+            HTMLAnchorElement
+                debugger
+                
         }
         
 
