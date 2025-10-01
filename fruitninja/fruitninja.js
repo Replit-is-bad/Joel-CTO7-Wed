d06f730e8091047aa04995400fcab7509f1102aa
Replit-is-bad <ylchia_hill@hotmail.com>
2025-10-01 08:29:37 +0000

    Oct 1, 2025, 8:29 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -41,5 +41,5 @@ function spawnFruit() {
     let fruitData = random(fruitTypes);
     let randomX = random(300, 500);
 
-    let fruit = new fruitGroup.Sprite(randomX,)
+    let fruit = new fruitGroup.Sprite(randomX, height+20)
 }
