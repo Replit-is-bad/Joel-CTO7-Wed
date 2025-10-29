1cbeaec8bb38554d2ec53a6d634c4b3a87096b1e
Replit-is-bad <ylchia_hill@hotmail.com>
2025-10-29 08:57:07 +0000

    Oct 29, 2025, 8:57 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -13,7 +13,7 @@ let gameTimer = 0;
 let gameDuration = 10;
 let bgtrack;
 let comboTrack;
-let difficultyNUM;
+let difficultyNumFruits = 1;
 
 function preload() {
     dojoBG = loadImage('assets/dojobackground.png');
