13d02db3345a1cb10448012f90b7b2c0900339f5
Replit-is-bad <ylchia_hill@hotmail.com>
2025-10-29 08:44:29 +0000

    Oct 29, 2025, 8:44 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -90,7 +90,8 @@ function draw() {
         textAlign(CENTER, CENTER); 
         text('Fruit Ninja', width/2, height/2-40); 
         textSize(24); 
-        text('Press space or click to start', width/2, height/2 +20); 
+        text('Press space or click to start', width/2, height/2 +20);
+         
 
         return; 
     }
