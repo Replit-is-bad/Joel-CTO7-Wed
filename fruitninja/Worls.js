6e2730e5c488e39aed3a0c022548ba13f21fa702
Replit-is-bad <ylchia_hill@hotmail.com>
2025-11-05 08:25:36 +0000

    Nov 5, 2025, 8:25 AM

--- fruitninja/Worls.js
+++ fruitninja/Worls.js
@@ -6,7 +6,7 @@ let button;
 
 function setup(){
     new Canvas(600,400);
-    
+
     textBox = createInput();
     textBox.position(550 + width/2 - textBox.width/2,300)
 
@@ -19,13 +19,5 @@ function draw() {
     textSize(24);
     textAlign(CENTER)
     text('Guess the Word!' , width/2, 50);
+    text('Guess the Word!' , width/2, 50);
 }
-
-
-
-textBox = createInput();
-textBox.position(550 + width/2 - textBox.width/2,300)
-
-
-button = createButton('Guess');
-button.position(textBox.x + textBox.width +30,300);
