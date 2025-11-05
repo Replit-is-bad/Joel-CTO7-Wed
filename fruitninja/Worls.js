6df96f40e56cd41802ab851dcf0227766915987c
Replit-is-bad <ylchia_hill@hotmail.com>
2025-11-05 08:39:27 +0000

    Nov 5, 2025, 8:39 AM

--- fruitninja/Worls.js
+++ fruitninja/Worls.js
@@ -1,35 +1,76 @@
-let word = ["Balls"];
-let attempt = 0;
-let hiddenW = '';
-let textBox;
-let button;
-let display;
+// let word = ["Balls"];
+// let attempt = 0;
+// let hiddenW = '';
+// let textBox;
+// let button;
+// let display;
 
-function setup(){
-    new Canvas(600,400);
+// function setup(){
+//     new Canvas(600,400);
 
-    textBox = createInput();
-    textBox.position(250 + width/2 - textBox.width/2,300)
+//     textBox = createInput();
+//     textBox.position(250 + width/2 - textBox.width/2,300)
 
-    button = createButton('Guess');
-    button.position(textBox.x + textBox.width +30,300);
-    button.mousePressed(displayText);
+//     button = createButton('Guess');
+//     button.position(textBox.x + textBox.width +30,300);
+//     button.mousePressed(displayText);
 
-    hiddenW = random(word);
-    console.log(hiddenW);
-    hiddenW = hiddenW[0].toUpperCase() +' '+ '_ '.repeat(hiddenW.length-1);
+//     hiddenW = random(word);
+//     console.log(hiddenW);
+//     hiddenW = hiddenW[0].toUpperCase() +' '+ '_ '.repeat(hiddenW.length-1);
+// }
+
+// function draw() {
+//     background(220);
+//     textSize(24);
+//     textAlign(CENTER)
+//     text('Guess the Word!' , width/2, 50);
+//     text('Attempts: ' ,attempt, width/2, 100);
+//     text('Hints : ' ,hiddenW, width/2, 150);
+//     text(display , width/2 , 350);
+// }
+
+// function displayText() {
+//     display = textBox.value();
+// }
+
+////////////////////////////////////////////////////////
+
+let words = ["grape", "apple", "pears", "mango", "kiwis"]; 
+let attempt = 0; 
+let hiddenWord = ""; 
+let textBox; 
+let button; 
+let display; 
+
+function setup() {
+    new Canvas(600, 400); 
+
+    textBox = createInput(); 
+    textBox.position(width/2 - textBox.width/2 + 250, 300); 
+
+    button = createButton('Guess'); 
+    button.position(textBox.x + textBox.width + 30, 300);
+    button.mousePressed(displayText); 
+
+    hiddenWord = random(words); 
+    console.log(hiddenWord);
+    hiddenWord = hiddenWord[0].toUpperCase() + " " + "_ ".repeat(hiddenWord.length-1); 
+
 }
 
+
 function draw() {
-    background(220);
-    textSize(24);
-    textAlign(CENTER)
-    text('Guess the Word!' , width/2, 50);
-    text('Attempts: ' ,attempt, width/2, 100);
-    text('Hints : ' ,hiddenW, width/2, 150);
-    text(display , width/2 , 350);
+    background(220); 
+    textSize(24); 
+    textAlign(CENTER); 
+    text("Guess the Word!", width/2, 50); 
+    text("Attempts: " + attempt, width/2, 100); 
+    text("Hint: " + hiddenWord, width/2, 150);
+    text(display, width/2, 320);
+
 }
 
 function displayText() {
-    display = textBox.value();
+    display = textBox.value(); 
 }
